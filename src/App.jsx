ac3d2fbb3cfe8aed6712bcd570bd4f4b6c7e727c
juvi-160 <juveriayameen160@gmail.com>
2025-03-21 15:07:43 +0530

    adding

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1 className="text-6xl">Hello this is the my first react app</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
